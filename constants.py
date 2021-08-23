eb3ca1ad6c0f622a3f41f24685837bc14bafcb94
Alexey Ozerov <alexey.ozerov@me.com>
2021-08-24 00:50:26 +0300

    Updated Constants

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ RESOURCES_DIR = os.path.join(BASE_DIR, "resources")
 WEB_DIR = os.path.join(RESOURCES_DIR, "web")
 LOG_DIR = os.path.join(BASE_DIR, "log")
 
-OFFLINE_STATE = "offline"
-ONLINE_STATE = "online"
+OFFLINE_STATE = "Offline"
+ONLINE_STATE = "Online"
 
 DISCOVERY_PERIOD_SEC = int(os.environ.get("DISCOVERY_PERIOD_SEC", default=30))
